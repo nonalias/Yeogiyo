2f82e563f0239f297422b88c9841be837fc1c44e
feldblume5263 <feldblume5263@gmail.com>
2020-12-16 15:11:28 +0900

    make project

---
+++ yeogiyo/yeogiyo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  yeogiyo
+//
+//  Created by Junhong Park on 2020/12/16.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
