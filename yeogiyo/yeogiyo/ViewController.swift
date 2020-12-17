0d1653edf15084738aae178a8898260c53ec85b7
nonalias <ktw1532@naver.com>
2020-12-17 19:11:05 +0900

    cluster parsing

--- yeogiyo/yeogiyo/ViewController.swift
+++ yeogiyo/yeogiyo/ViewController.swift
@@ -8,89 +8,6 @@
 import UIKit
 let session: URLSession = URLSession.shared
 
-func get_token(uid: String, secret: String) -> Any
-{
-    let urlStr : String = "https://api.intra.42.fr/oauth/token"
-    let url = URL(string: urlStr)
-    let sem = DispatchSemaphore.init(value: 0)
-    var request: URLRequest = URLRequest(url: url!)
-    request.httpMethod = "POST"
-    let bodyData = "grant_type=client_credentials&client_id=" + uid + "&client_secret=" + secret
-    //print(bodyData)
-    request.httpBody = bodyData.data(using: String.Encoding.utf8)
-    var token : Any?
-    session.dataTask(with: request, completionHandler: { (data, response, error) in
-        defer { sem.signal() }
-        guard let data = data else { print("nothing");return }
-
-        let json = try? JSONSerialization.jsonObject(with: data, options: []) as? [String:Any]
-        print(json)
-        token = json?["access_token"]
-    }).resume()
-    sem.wait()
-    return token
-}
-
-func getCluster(url: URL, token: String) -> [[String: Any]]? {
-    var request: URLRequest = URLRequest(url: url)
-    request.httpMethod = "GET"
-    let sem = DispatchSemaphore.init(value: 0)
-    let appMessage = " Bearer " + token
-    request.setValue(appMessage, forHTTPHeaderField: "Authorization")
-    request.addValue("application/json; charset=utf-8", forHTTPHeaderField: "Content-Type")
-    var json: [[String: Any]]?
-    session.dataTask(with: request, completionHandler: { (data, response, error) in
-        defer { sem.signal() }
-        guard let data = data else { print("nothing");return }
-        json = try? JSONSerialization.jsonObject(with: data, options: []) as? [[String: Any]]
-    }).resume()
-    sem.wait()
-    return json
-}
-
-func clusterProc(c: [User], jsonCluster: [[String: Any]]?)
-{
-    var i : Int
-    var j : Int
-    
-    var jsonCount : Int
-    jsonCount = (jsonCluster?.count)!
-    i = 1
-    while i < c.count
-    {
-        j = 0
-        while j < jsonCount - 1
-        {
-            var host : String? = jsonCluster?[j]["host"] as? String
-            var end_at : String? = jsonCluster?[j]["end_at"] as? String
-            if (c[i].compareSeats(host: host!) && end_at == nil)
-            {
-                var usrDic: NSDictionary? = (jsonCluster?[j]["user"])! as? NSDictionary
-                var usrId : Int? = usrDic?["id"] as? Int
-                var usrLogin : String? = usrDic?["login"] as? String
-                var usrUrl : String? = usrDic?["url"] as? String
-                c[i].setValidUser(id: usrId!, host: host!, url: usrUrl!, login: usrLogin!)
-                c[i].showAll()
-            }
-            j += 1
-        }
-        i += 1
-    }
-}
-
-func getClusterIndexByHost(c: [User], host: String) -> Int {
-    var ret = 1
-    while ret < c.count
-    {
-        if (c[ret].compareSeats(host: host))
-        {
-            return ret
-        }
-        ret += 1
-    }
-    return (-1)
-}
-
 class User {
     var id : Int?
     var c : Int
@@ -137,37 +54,6 @@ class User {
         print(self.id)
         print(self.host)
         print("==end===")
-    }
-}
-
-func initializeCluster(c : inout [User], clusterNumber: Int)
-{
-    var i : Int = 1
-    var j : Int = 1
-    
-    if (clusterNumber % 2 == 1)
-    {
-        i = 1
-        while i < 10 {
-            j = 1
-            while j < 8 {
-                c.append(User(c: clusterNumber, r: i, s: j))
-                j += 1
-            }
-            i += 1
-        }
-    }
-    else
-    {
-        i = 1
-        while i < 11 {
-            j = 1
-            while j < 9 {
-                c.append(User(c: clusterNumber, r: i, s: j))
-                j += 1
-            }
-            i += 1
-        }
     }
 }
 
